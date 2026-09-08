90d53cf8563b0f0651f9426299476ad3820af16b
Anas Amrouche <amrouche.an@gmail.com>
2026-09-08 15:12:48 +0200

    Importation dans init.py

--- src/textstats/__init__.py
+++ src/textstats/__init__.py
@@ -1,2 +1,7 @@
-def hello() -> str:
-    return "Hello from textstats!"
+from textstats.core import char_frequencies, longest_word, word_count
+
+__all__ = [
+    "char_frequencies",
+    "longest_word",
+    "word_count",
+]
